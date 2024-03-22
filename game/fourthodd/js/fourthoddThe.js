efc0974d6ac3b01a129b63ec61784b11806d36b3
marina9290 <marina9290@yandex.ru>
2024-03-22 12:03:27 +0300

    feat fourhodd

---
+++ game/fourthodd/js/fourthoddThe.js
@@ -0,0 +1,16 @@
+const result = document.querySelector('#result');
+const wrong1 = document.querySelector('#wrong1');
+const wrong2 = document.querySelector('#wrong2');
+const wrong4 = document.querySelector('#wrong4');
+const right = document.querySelector('#right');
+const show = document.querySelector('#show');
+
+result.addEventListener('click', (e) => {
+    e.preventDefault();
+    right.classList.add("right-answer");
+    wrong1.classList.add("wrong-answer");
+    wrong2.classList.add("wrong-answer");
+    wrong4.classList.add("wrong-answer");
+    result.classList.add('hidden');
+    show.textContent = 'Сани лишние, потому что они не еда.'
+});
