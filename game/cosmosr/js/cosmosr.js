4c5819cdac3965174f953e806b37b3fe11e34cbd
marina9290 <marina9290@yandex.ru>
2024-03-31 19:07:50 +0300

    feat cosmos js merc

---
+++ game/cosmosr/js/cosmosr.js
@@ -0,0 +1,13 @@
+const mercure = document.querySelector('#mercure');
+const redMerc = document.querySelector('#redMerc');
+const rocket = document.querySelector('#rocket');
+const poem = document.querySelector('#poem');
+
+redMerc.addEventListener('click', (e) => {
+    rocket.classList.add("move");
+    
+    setTimeout(function(){ 
+        flag.classList.add("show");
+        poem.classList.add("show");
+    }, 4000);
+});
