ee87e63462dad1f98d4c51c4f5d226cea131e069
marina9290 <marina9290@yandex.ru>
2024-04-01 18:38:50 +0300

    feat audio cosmos

--- game/cosmosr/js/cosmosr.js
+++ game/cosmosr/js/cosmosr.js
@@ -2,13 +2,22 @@ const mercure = document.querySelector('#mercure');
 const redMerc = document.querySelector('#redMerc');
 const rocket = document.querySelector('#rocket');
 const poem = document.querySelector('#poem');
+const audioBtn = document.querySelector('#audioBtn');
+const audioRocket = document.querySelector('#audioRocket');
 
 redMerc.addEventListener('click', (e) => {
     rocket.classList.add("move");
-    
+    audioBtn.play();
+    setTimeout(function(){ 
+        audioRocket.play();
+    }, 500);
     setTimeout(function(){ 
         flag.classList.add("show");
         poem.classList.add("show");
     }, 4000);
 });
+
+
+// oneAudio.play();
+// twoAudio.pause();
 
